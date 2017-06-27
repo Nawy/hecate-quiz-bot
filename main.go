b530f72a1ccca04b523ddba0da5f79fd9171ac51
Ivan Ermolaev <ermolaevym@gmail.com>
2017-06-28 01:19:06 +0300

    simple using

--- main.go
+++ main.go
@@ -1,58 +1,66 @@
-package hecate_quiz_bot
+package main
 
 import (
-	"gopkg.in/telegram-bot-api.v4"
 	"log"
-	"io/ioutil"
-	"gopkg.in/yaml.v2"
-	"os"
+
+	"gopkg.in/telegram-bot-api.v4"
 )
 
-type AppConfigYAML struct {
-	Bot struct {
-		Name  string `yaml:"name"`
-		Debug bool `yaml:"debug"`
-		Networks struct {
-			Token string `yaml:"token"`
-			Host  string `yaml:"host"`
-			Port  string `yaml:"port"`
-		}`yaml:"networks"`
-	} `yaml:"bot"`
-}
-
-var conf AppConfigYAML = AppConfigYAML{}
-
-func initConfig() {
-	configPath := getPathFromArgs()
-	yamlFile, err := ioutil.ReadFile(configPath)
-	if err != nil {
-		panic("Not found config.yaml")
-	}
-
-	err = yaml.Unmarshal(yamlFile, &conf)
-	if err != nil {
-		panic("Cannot unmashal config")
-	}
-}
-
-func getPathFromArgs() string {
-	args := os.Args[1:]
-	for i := 0; i < len(args); i++ {
-		if args[i] == "-c" {
-			return args[i+1]
-		}
-	}
-	panic("cannot read config from -c option")
-}
+// func main() {
+// 	InitConfig()
+// 	InitLogger()
+//
+// 	bot, err := tgbotapi.NewBotAPI(conf.Bot.Networks.Token)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+//
+// 	bot.Debug = conf.Bot.Debug
+//
+// 	webhookURL := GetWebhookURL(conf.Bot.Networks.External)
+// 	webhook := tgbotapi.NewWebhookWithCert(webhookURL, conf.Bot.Networks.Ssl.Cert)
+//
+// 	_, err = bot.SetWebhook(webhook)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+//
+// 	updates := bot.ListenForWebhook("/" + conf.Bot.Networks.Token)
+//
+// 	listenURL := GetWebhookURL(conf.Bot.Networks.Internal)
+// 	go http.ListenAndServeTLS(listenURL, conf.Bot.Networks.Ssl.Cert, conf.Bot.Networks.Ssl.Key, nil)
+//
+// 	for update := range updates {
+// 		log.Printf("%+v\n", update)
+// 	}
+// }
 
 func main() {
-	initConfig()
+	InitConfig()
+	defer InitLogger().Close()
 
 	bot, err := tgbotapi.NewBotAPI(conf.Bot.Networks.Token)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	bot.Debug = conf.Bot.Debug
 
-	if err != nil {
-		log.Fatal(err)
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		userName := update.Message.From.UserName
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.Text = "Как твое имя " + userName + "?"
+		bot.Send(msg)
 	}
 }
